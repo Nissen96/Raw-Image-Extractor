6010d573594afed9f866da7fd09fbd1a8c23e31e
Nissen <alexander@feldbech.dk>
2024-03-26 10:32:09 +0100

    Initial script

---
+++ README.md
@@ -0,0 +1,52 @@
+# Raw Image Extractor
+
+Script for extracting raw image data as images from binary files.
+
+Can be used on any binary data, but very useful for memory dumps, specifically process memory dumped with e.g. Volatility.
+This makes it easy to e.g. dump the process memory of Paint and see the drawing,
+or dump basically any process and get a screenshot of the entire screen.
+
+Not recommended for use on entire memory dumps, the smaller the binary file, the faster the process and the fewer the results.
+
+## Installation
+
+Make sure Python is installed, dependencies can be installed with
+
+```
+python -m pip install -r requirements.txt
+```
+
+## Usage
+
+Only an input file is required, but image width, height and mode (RGB/RGBA) can be provided if known, reducing the number of outputs:
+
+```
+usage: extract-images.py [-h] [--width WIDTH] [--height HEIGHT] [--mode {RGB,RGBA}]
+                  [--out OUT] [--offset OFFSET]
+                  infile
+
+Extract image from raw data
+
+positional arguments:
+  infile             Raw data input file
+
+options:
+  -h, --help         Show this help message and exit
+  --width WIDTH      Image width
+  --height HEIGHT    Image height
+  --mode {RGB,RGBA}  Image mode
+  --out OUT          Output directory (default: output/)
+  --offset OFFSET    Data offset
+```
+
+If `--width` is not set, the script is repeated for a number of standard screen resolutions `(800, 1024, 1280, 1366, 1440, 1600, 1920, 2048)`,
+extracting images for each option - might take a while but is useful if width is unknown.
+
+If `--mode` is not set, the script will run all extractions for both RGB- and RGBA-mode.
+
+If `--height` is not set, only a single, tall image is produced (based on the width and mode), visualizing the enitre memory at once.
+Otherwise, the memory is split into `width x height x channels` chunks and an image extracted for each.
+
+`--offset` can be set to specify a specific data offset where the extraction should begin.
+
+Results are stored in the specified output directory, possibly split into subfolders based on resolution and channel.
---
+++ extract-images.py
@@ -0,0 +1,73 @@
+import argparse
+import numpy as np
+import os
+import sys
+import typing
+
+from pathlib import Path
+from PIL import Image
+from tqdm import trange
+
+
+COMMON_WIDTHS = (800, 1024, 1280, 1366, 1440, 1600, 1920, 2048)
+MODES = ("RGB", "RGBA")
+
+
+def extract_images(fd: typing.BinaryIO, total_size: int, width: int, height: int, mode: str, offset: int, outdir: Path) -> None:
+    channels = len(mode)
+    if height is None:
+        height = total_size // (width * channels)
+    
+    size = width * height * channels
+    nimages = total_size // size
+
+    print(f"Extracting {nimages} {mode}-image{'s' if nimages > 1 else ''} ({width}x{height})...")
+    fd.seek(offset)
+    for i in trange(total_size // size):
+        data = np.uint8(list(fd.read(size)))
+        img = Image.fromarray(data.reshape((height, width, len(mode))))
+        if all(minval == maxval for minval, maxval in img.getextrema()):
+            continue
+        
+        path = outdir
+        filename = "img"
+        if nimages > 1:
+            path = path / f"{width}x{height}" / mode
+            filename += f"-{i * size}.png"
+        else:
+            filename += f"-{width}x{height}-{mode}.png"
+
+        path.mkdir(parents=True, exist_ok=True)
+        img.save(path / filename)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Extract image from raw data")
+    parser.add_argument("infile", type=argparse.FileType("rb"), default=sys.stdin, help="Raw data input file")
+    parser.add_argument("--width", type=int, help="Image width")
+    parser.add_argument("--height", type=int, help="Image height")
+    parser.add_argument("--mode", type=str, choices=MODES, help="Image mode")
+    parser.add_argument("--out", default="output", help="Output directory (default: output/)")
+    parser.add_argument("--offset", type=int, default=0, help="Data offset")
+
+    args = parser.parse_args()
+    args.widths = (args.width,) if args.width is not None else COMMON_WIDTHS
+    args.modes = (args.mode,) if args.mode is not None else MODES
+    args.out = Path(args.out)
+
+    args.infile.seek(0, os.SEEK_END)
+    args.length = args.infile.tell() - args.offset
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+
+    for width in args.widths:
+        for mode in args.modes:
+            extract_images(args.infile, args.length, width, args.height, mode, args.offset, args.out)
+            print()
+
+
+if __name__ == "__main__":
+    main()
